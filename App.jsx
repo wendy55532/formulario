2226507bfc0da553029820c6d47a5230bbf22ef0
wendy55532 <135014973+wendy55532@users.noreply.github.com>
2024-06-22 19:02:07 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,25 @@
+
+
+function App ( ) {
+  
+  return (
+    <form >
+<label htmlFor="Nombre"> Nombre </label>
+
+<input type="text"></input>
+
+<label htmlFor="correo"> Correo</label>
+<input type="Email"></input>
+
+<label htmlFor="pasword"> Contraseña</label>
+<input type="Pasword"></input>
+
+<label htmlFor="confirmarpasword">Confirmar  Contraseña</label>
+<input type="Pasword"></input>
+
+<button> Submit</button>
+
+    </form>
+  )
+}
+export default App;
